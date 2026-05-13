9c3068a77daf33fd25afc945fbae9f901f404d70
Adithya-Ranjit13 <adithyaranjit1313@gmail.com>
2026-05-13 12:00:42 +0530

    Configure Supabase db connection

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,8 +5,8 @@ dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-
   datasource: {
     url: process.env.DATABASE_URL,
+    shadowDatabaseUrl: process.env.DIRECT_URL,
   },
 });
