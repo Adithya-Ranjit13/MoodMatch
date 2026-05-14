680994cbf436b4e645c5f15d5bcae5faf9d192df
Adithya-Ranjit13 <adithyaranjit1313@gmail.com>
2026-05-14 14:32:04 +0530

    Changed from google smtp to brevo smtp as render blocks port in google

--- backend/src/lib/email.ts
+++ backend/src/lib/email.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: Number(process.env.SMTP_PORT) === 465,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
